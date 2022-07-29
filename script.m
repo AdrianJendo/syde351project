c1ed54b64e7cad0c7bcfdf4aa2a48ef7044467a7
Adam Barroso <abarroso@edu.uwaterloo.ca>
2022-07-29 01:14:22 -0400

    fixed constants, added graphs

--- script.m
+++ script.m
@@ -2,18 +2,17 @@
 %C42-L90
 N = Simulink.Parameter(2.5); %gear ratio
 Kb = Simulink.Parameter(0.5730); %vbemf ratio
-Kt = Simulink.Parameter(0.42); %torque const
+Kt = Simulink.Parameter(0.5791); %torque const
 La = Simulink.Parameter(0.0054); %induct
 Ra = Simulink.Parameter(1.45);%
 R = Simulink.Parameter(0.05);
 %TOTl mASS
-M = Simulink.Parameter(47.428); 
+M = Simulink.Parameter(54.856); 
 L = Simulink.Parameter(0.5);%aSSUME WHEEL STICKS OUT
 d = Simulink.Parameter(0);
-J = Simulink.Parameter(2.720117);
+J = Simulink.Parameter(1.71425);
 theta = Simulink.Parameter(0);
-%9.81   ×   ( 47.428   ÷   2 )   ×   0.45   ×   0.05 *2
-td= Simulink.Parameter(2.72011);
+
 
 
 % Tdl = Simulink.Parameter(1);
